525de0f2845b085c92fa5d4de75eafe4bf02e28f
Prim69 <almusawihashim@gmail.com>
2020-11-16 17:15:53 -0500

    Add files via upload

---
+++ src/Prim69/AdvancedPots/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Prim69\AdvancedPots;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\Player;
+use pocketmine\entity\projectile\SplashPotion;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\ProjectileHitBlockEvent;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onHit(ProjectileHitBlockEvent $event){
+		$projectile = $event->getEntity();
+		if(!$projectile instanceof SplashPotion) return;
+		$player = $projectile->getOwningEntity;
+		if(!$player) return;
+		$distance = $projectile->distance($player);
+		if($player instanceof Player && $distance <= 3 && $player->isAlive())
+		    $player->setHealth($player->getHealth() + 5);
+	}
+
+}
