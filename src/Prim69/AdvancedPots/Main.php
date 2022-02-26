79870c4545cf3ea11035c88ea2aeaf5873ff4f43
Hashim <13991048+Prim69@users.noreply.github.com>
2022-02-25 19:58:58 -0500

    update to pm4

--- src/Prim69/AdvancedPots/Main.php
+++ src/Prim69/AdvancedPots/Main.php
@@ -4,26 +4,26 @@ namespace Prim69\AdvancedPots;
 
 use pocketmine\plugin\PluginBase;
 
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\entity\projectile\SplashPotion;
 use pocketmine\event\Listener;
+use pocketmine\item\PotionType;
 use pocketmine\event\entity\ProjectileHitBlockEvent;
 
 class Main extends PluginBase implements Listener{
 
-	public function onEnable(){
+	public function onEnable() : void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
 	public function onHit(ProjectileHitBlockEvent $event){
 		$projectile = $event->getEntity();
-		if(!$projectile instanceof SplashPotion) return;
-		if($projectile->getPotionId() !== 22) return;
-		$player = $projectile->getOwningEntity();
-		if(!$player) return;
-		$distance = $projectile->distance($player);
-		if($player instanceof Player && $distance <= 3 && $player->isAlive())
-		    $player->setHealth($player->getHealth() + 5);
+		if($projectile instanceof SplashPotion && $projectile->getPotionType() === PotionType::STRONG_HEALING()){
+			$player = $projectile->getOwningEntity();
+			if($player instanceof Player && $player->isAlive() && $projectile->getPosition()->distance($player->getPosition()) <= 3){
+				$player->setHealth($player->getHealth() + 5);
+			}
+		}
 	}
 
 }
